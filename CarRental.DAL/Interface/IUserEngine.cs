a81c62bd168faede04d237f89e301c16f4f60ba7
William's <modeste.william.s@gmail.com>
2019-08-09 20:44:43 +0200

    ajout de l'interface IuserEngine
    méthodes get list et update

---
+++ CarRental.DAL/Interface/IUserEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Model;
+
+namespace CarRental.DAL.Interface
+{
+    interface IUserEngine
+    {
+        UserDTO Get(string email);
+        List<UserDTO> List();
+        void update(UserDTO user);
+    }
+}
