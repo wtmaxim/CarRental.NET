7a928c06c8231d1b5f4e8fc04c6569365eac4670
Unknown <modeste.william.s@gmail.com>
2019-10-19 21:36:06 +0200

    ajout de rôle fonctionnel

--- CarRental.UI/Controllers/RoleController.cs
+++ CarRental.UI/Controllers/RoleController.cs
@@ -25,8 +25,7 @@ namespace CarRental.UI.Controllers
         /// <returns></returns>
         public ActionResult Edit(int id)
         {
-            if (id != 0)
-            {
+           
                 RoleDTO roleToEdit = roleLogic.Get_By_Id(id);
                 List<ActionDTO> roleToEditActions = actionLogic.get_Role_Actions(roleToEdit);
                 RoleEditViewModel REVM = new RoleEditViewModel()
@@ -36,21 +35,69 @@ namespace CarRental.UI.Controllers
                 };
 
                 return View(REVM);
-            }
-            
-            return View();
+                       
         }
-
+        /// <summary>
+        /// Met a jour un rôle.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="roles"></param>
+        /// <param name="roleName"></param>
+        /// <returns></returns>
         [HttpPost]
-        public ActionResult Edit(int id, string[] roles,string roleName)
+        public ActionResult Edit(int id, string[] actions,string roleName)
         {
             if (roleName != null)
             {
                 roleLogic.update(id, roleName);
             }
-            roleLogic.Set_Role_Actions(roleLogic.Get_Role_And_Actions_By_Ids(id, roles));            
+            roleLogic.Set_Role_Actions(roleLogic.Get_Role_And_Actions_By_Ids(id, actions));            
             return RedirectToAction("Index","Configuration");
         }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            RoleDTO role = new RoleDTO();
+            List <ActionDTO> actions = new List<ActionDTO>();
+            RoleEditViewModel REVM = new RoleEditViewModel()
+            {
+                RoleWithActionTuple = new Tuple<RoleDTO, List<ActionDTO>>(role, actions),
+                allActions = actionLogic.List()
+            };
+
+            return View(REVM);
+        }
+
+        [HttpPost]
+        public ActionResult Add (string[] actions, string roleName)
+        {
+            
+           
+             // Si aucune role avec ce nom n'existe, je l'ajoute en base.
+            if(roleLogic.List().Find(u => u.Libelle == roleName) == null)
+            {
+                roleLogic.AddRole(roleName);
+                roleLogic.Set_Role_Actions(roleLogic.Get_Role_And_Actions_By_Ids(roleLogic.Get(roleName).Id, actions));
+                return RedirectToAction("Index", "Configuration");
+            }
+            else
+            {
+                ViewBag.message = "Il existe déjà un role disposant de son libellé.";
+                ViewBag.status = true;
+                RoleDTO role = new RoleDTO();
+                List<ActionDTO> listActions = new List<ActionDTO>();
+                RoleEditViewModel REVM = new RoleEditViewModel()
+                {
+                    RoleWithActionTuple = new Tuple<RoleDTO, List<ActionDTO>>(role, listActions),
+                    allActions = actionLogic.List()
+                };
+                return View(REVM);
+
+            }           
+
+          
+        }
+
 
         /// <summary>
         /// Supprime un role
