5c048def9a84d9e3fce6d16ccf9700b8adaebe31
Maxime DERAME <maxime.derame@hotmail.fr>
2019-11-20 11:54:01 +0100

    f
    v

---
+++ Database/5. Data_Status.sql
@@ -0,0 +1,5 @@
+
+INSERT [dbo].[Status] ([Id], [Libelle]) VALUES (2, N'Accepté')
+INSERT [dbo].[Status] ([Id], [Libelle]) VALUES (1, N'En acceptation')
+INSERT [dbo].[Status] ([Id], [Libelle]) VALUES (1002, N'Refusé')
+INSERT [dbo].[Status] ([Id], [Libelle]) VALUES (2002, N'Terminé')
